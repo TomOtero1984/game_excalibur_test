f34dba1d46b49f544b6335a5359151febadc1268
Tom Otero <tomotero1984@gmail.com>
2021-05-09 20:23:29 -0700

    adding browserify

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static(__dirname));
+
 app.get('/', (req, res) => {
-  res.sendFile('index.html', { root: __dirname });
+    res.sendFile('index.html', { root: __dirname });
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
